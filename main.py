76f9e15c476718fdd2615fac21d30d65d64c4f7d
Kayra2337 <161927412+Kayra2337@users.noreply.github.com>
2025-05-18 10:07:08 +0300

    main.py

--- main.py
+++ main.py
@@ -14,7 +14,17 @@ c = conn.cursor()
 c.execute("SELECT * FROM haberler ORDER BY trend_id LIMIT 99")
 haberler = c.fetchall()
 
-for i in range(len(haberler)):
-    st.image(haberler[i][3])
-    st.write(haberler[i][1])
-    st.link_button("habelere git", haberler[i][2])
+for i in range(0,len(haberler)):
+    col1,col2,col3=st.colums(3)
+    with col1:
+        st.image(haberler[i][3])
+        st.write(haberler[i][1])
+        st.link_button("habelere git", haberler[i][2])
+    with col2:
+        st.image(haberler[i+1][3])
+        st.write(haberler[i+1][1])
+        st.link_button("habelere git", haberler[i+1][2])
+    with col3:
+        st.image(haberler[i+2][3])
+        st.write(haberler[i+2][1])
+        st.link_button("habelere git", haberler[i+2][2])
